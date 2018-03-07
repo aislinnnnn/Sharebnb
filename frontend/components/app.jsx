598de323b0a6b0e0c567837bb4ad0edb1d1e9da3
Aislinn Cronin <aislinn.cronin2@gmail.com>
2018-03-07 14:02:27 -0500

    Add image and styling to homepage

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import GreetingContainer from './greeting/greeting_container';
-import { Route } from 'react-router-dom';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
-import {AuthRoute} from '../util/route_util';
+import React from "react";
+import GreetingContainer from "./greeting/greeting_container";
+import { Route } from "react-router-dom";
+import LoginFormContainer from "./session/login_form_container";
+import SignupFormContainer from "./session/signup_form_container";
+import { AuthRoute } from "../util/route_util";
 
 const App = () => (
   <div>
@@ -12,6 +12,10 @@ const App = () => (
     </header>
     <AuthRoute path="/login" component={LoginFormContainer} />
     <AuthRoute path="/signup" component={SignupFormContainer} />
+    <img
+      className="app-image"
+      src="https://www.holidayguru.ie/wp-content/uploads/2017/01/Galway-2000px.jpg"
+    />
   </div>
 );
 
--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -10,9 +10,6 @@ class SessionForm extends React.Component {
     };
   }
 
-
-
-
   handleSubmit(e) {
     // debugger
     e.preventDefault();
@@ -21,50 +18,55 @@ class SessionForm extends React.Component {
   }
 
   update(field) {
-    return (e) => this.setState({[field]:e.currentTarget.value});
+    return e => this.setState({ [field]: e.currentTarget.value });
   }
 
   renderErrors() {
-    return(
+    return (
       <ul>
         {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
-            {error}
-          </li>
+          <li key={`error-${i}`}>{error}</li>
         ))}
       </ul>
     );
   }
 
+  // signupInputs() {
+  //   <input ...
+  // }
+
   render() {
     return (
       <div className="session-form">
-      <div className="form-input">
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <h1 className="greeting">{this.props.greeting}</h1>
-          {this.renderErrors()}
+        <div className="form-input">
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <h1 className="greeting">{this.props.greeting}</h1>
+            {this.renderErrors()}
             <label>
-              <input type="text"
+              <input
+                type="text"
                 value={this.state.username}
-                onChange={this.update('username')}
-                />
+                onChange={this.update("username")}
+              />
             </label>
             <label>
-              <input type="text"
+              <input
+                type="text"
                 value={this.state.password}
-                onChange={this.update('password')}
-                />
+                onChange={this.update("password")}
+              />
             </label>
-          <button>{this.props.formType}</button>
-        </form>
-        <div className="foot">
-          <span>{this.props.text}</span>
-          {this.props.link}
-        </div>
+            <button>{this.props.formType}</button>
+          </form>
+          <div className="foot">
+            <span>{this.props.text}</span>
+            {this.props.link}
+          </div>
         </div>
       </div>
     );
   }
 }
+// { this.props.formtype === signup ? this.signupInputs() : null }
 
 export default withRouter(SessionForm);
