9fa553fe50b0cd7bef44c7d63018b9e25c7525d0
Aislinn Cronin <aislinn.cronin2@gmail.com>
2018-03-10 14:44:53 -0500

    Begin work on filters for map

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,4 +3,8 @@ class Spot < ApplicationRecord
   has_attached_file :image, default_url: 'home-default.jpg'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  def self.in_bounds(bounds)
+    
+  end
+
 end
