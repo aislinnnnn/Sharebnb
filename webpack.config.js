f5fa72521d7d33c6f58153092ca2f015ad70ebf8
Aislinn <aislinn.cronin2@gmail.com>
2018-03-21 13:30:51 -0400

    Fix bug in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require("path");
 var webpack = require("webpack");
 
-var plugins = [];
-var devPlugins = [];
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
   new webpack.DefinePlugin({
@@ -10,7 +10,7 @@ var prodPlugins = [
       'NODE_ENV': JSON.stringify('production')
     }
   }),
-  new webpack.optimize.UglifyPlugin({
+  new webpack.optimize.UglifyJsPlugin({
     compress: {
       warnings: true
     }
@@ -19,7 +19,8 @@ var prodPlugins = [
 
 plugins = plugins.concat(
   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-)
+);
+
 
 module.exports = {
   context: __dirname,
